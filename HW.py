58cc07317066c62ff082ff9e00ad8435bb20426e
min-johnny <wlgusdl0408@naver.com>
2022-06-29 01:34:29 +0900

    first commit

---
+++ HW.py
@@ -0,0 +1,7 @@
+x1,y1=map(int,input().split())
+x2,y2=map(int,input().split())
+print('x1 :',x1)
+print('y1 :',y1)
+print('x2 :',x2)
+print('y2 :',y2)
+print('두 점 사이의 거리 =',(((x2-x1)**2+(y2-y1)**2)**(1/2)))
