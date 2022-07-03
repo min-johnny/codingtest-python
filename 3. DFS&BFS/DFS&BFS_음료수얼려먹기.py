2286f726a37c96342597c93f2a9460e66857d041
min-johnny <wlgusdl0408@naver.com>
2022-07-04 00:34:46 +0900

    dfs&bfs

---
+++ 3. DFS&BFS/DFS&BFS_음료수얼려먹기.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+def dfs(x, y):
+    # 주어진 범위 벗어나는 경우 종료
+    if x >= n or x < 0 or y >= m or y < 0:
+        return False
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        # 해당 노드 방문 처리
+        graph[x][y] = 1
+        # 상하좌우 방문
+        dfs(x+1, y)
+        dfs(x-1, y)
+        dfs(x, y+1)
+        dfs(x, y-1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j):
+            result += 1
+
+print(result)
